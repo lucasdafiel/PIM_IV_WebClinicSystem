8ac4ebce59ac826810fe043149397fe602a62bbc
Lucas Micael <lucasmicaellds@gmail.com>
2025-10-18 19:50:22 -0300

    Alterado o código com base nas tarefas do Trello, criado o DTO e o Controller da classe Paciente.

---
+++ WebClinicSystem/WebClinicSystem.Application/Features/Pacientes/DTOs/CreatePacienteDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClinicSystem.Application.Features.Pacientes.DTOs
+{
+    // Este 'record' serve para receber os dados da requisição para criar um paciente.
+    public record CreatePacienteDto(
+        string NomeCompleto,
+        string Cpf,
+        DateTime DataNascimento,
+        string TelefoneContato
+    );
+}
---
+++ WebClinicSystem/WebClinicSystem.Domain/Entities/Perfil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClinicSystem.Domain.Entities
+{
+    public class Perfil
+    {
+        // Chave primária da tabela Perfis.
+        public int PerfilId { get; private set; }
+
+        // Nome do perfil (ex: "Administrador").
+        public string Nome { get; private set; }
+    }
+}
